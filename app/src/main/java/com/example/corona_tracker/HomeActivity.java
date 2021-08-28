cf0dab340ae38271f2e32b71ae63041261e08cd9
vishal R <vishal206ani@gmail.com>
2021-08-28 22:13:02 +0530

    back-end completed

--- app/src/main/java/com/example/corona_tracker/HomeActivity.java
+++ app/src/main/java/com/example/corona_tracker/HomeActivity.java
@@ -2,7 +2,14 @@ package com.example.corona_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,9 +23,12 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private TextView tv_wwCases,tv_todayww;
+    private Spinner spinnerSelect;
+    private ImageButton ibtn_selectState;
+    private String valueFromSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +36,31 @@ public class HomeActivity extends AppCompatActivity {
 
         tv_wwCases=findViewById(R.id.tv_wwCases);
         tv_todayww=findViewById(R.id.tv_todayww);
+        spinnerSelect=findViewById(R.id.spinnerSelect);
+        ibtn_selectState=findViewById(R.id.ibtn_selectState);
+
+
+
+        String[] statesArray=getResources().getStringArray(R.array.states);
+        ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_spinner_item,statesArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSelect.setAdapter(adapter);
+
+        spinnerSelect.setOnItemSelectedListener(this);
+
+        ibtn_selectState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(valueFromSpinner.equals("-select state-")){
+                    Toast.makeText(HomeActivity.this, "select the state", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Intent intent=new Intent(HomeActivity.this,stateActivity.class);
+                    intent.putExtra("state",valueFromSpinner);
+                    startActivity(intent);
+                }
+            }
+        });
 
         fetchdata();
     }
@@ -56,5 +91,18 @@ public class HomeActivity extends AppCompatActivity {
                 });
         RequestQueue requestQueue= Volley.newRequestQueue(this);
         requestQueue.add(request);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(parent.getId()==R.id.spinnerSelect){
+            valueFromSpinner=parent.getItemAtPosition(position).toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/example/corona_tracker/stateActivity.java
+++ app/src/main/java/com/example/corona_tracker/stateActivity.java
@@ -3,12 +3,79 @@ package com.example.corona_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class stateActivity extends AppCompatActivity {
+
+    private TextView tv_total,tv_deceased,tv_recovered,tv_tested,tv_first,tv_second,tv_stateName;
+    private String valueFromSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state);
+
+        tv_total=findViewById(R.id.tv_total);
+        tv_deceased=findViewById(R.id.tv_deceased);
+        tv_recovered=findViewById(R.id.tv_recovered);
+        tv_tested=findViewById(R.id.tv_tested);
+        tv_first=findViewById(R.id.tv_first);
+        tv_second=findViewById(R.id.tv_second);
+
+        tv_stateName=findViewById(R.id.tv_stateName);
+
+
+
+        valueFromSpinner=(String)getIntent().getSerializableExtra("state");
+
+        tv_stateName.setText(valueFromSpinner);
+//
+//        Toast.makeText(this, valueFromSpinner, Toast.LENGTH_SHORT).show();
+
+        fetchdata();
+    }
+    private void fetchdata(){
+        String url = "https://data.covid19india.org/v4/min/data.min.json";
+
+        StringRequest request=new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject=new JSONObject(response.toString());
+
+                            tv_total.setText(jsonObject.getJSONObject(valueFromSpinner).getJSONObject("total").getString("confirmed").toString());
+                            tv_deceased.setText(jsonObject.getJSONObject(valueFromSpinner).getJSONObject("total").getString("deceased").toString());
+                            tv_recovered.setText(jsonObject.getJSONObject(valueFromSpinner).getJSONObject("total").getString("recovered").toString());
+                            tv_tested.setText(jsonObject.getJSONObject(valueFromSpinner).getJSONObject("total").getString("tested").toString());
+                            tv_first.setText(jsonObject.getJSONObject(valueFromSpinner).getJSONObject("total").getString("vaccinated1").toString());
+                            tv_second.setText(jsonObject.getJSONObject(valueFromSpinner).getJSONObject("total").getString("vaccinated2").toString());
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(stateActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
